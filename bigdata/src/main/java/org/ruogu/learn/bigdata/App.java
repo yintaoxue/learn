898c37b0f1c9ab4235a30b02056439b207bfe183
Talin <yintaoxue@gmail.com>
2016-03-11 23:55:11 +0800

    mr
    wordcount

---
+++ bigdata/src/main/java/org/ruogu/learn/bigdata/App.java
@@ -0,0 +1,13 @@
+package org.ruogu.learn.bigdata;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
