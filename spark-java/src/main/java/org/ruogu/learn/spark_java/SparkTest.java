62b529a1620fc2fa13104968cb596d710e3313da
Talin <yintaoxue@gmail.com>
2016-03-13 18:58:40 +0800

    spark and java lambda

--- spark-java/src/main/java/org/ruogu/learn/spark_java/SparkTest.java
+++ spark-java/src/main/java/org/ruogu/learn/spark_java/SparkTest.java
@@ -1,12 +1,14 @@
 package org.ruogu.learn.spark_java;
 
 import java.util.Arrays;
+import java.util.List;
 
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.FlatMapFunction;
+import org.apache.spark.api.java.function.Function;
 import org.apache.spark.api.java.function.Function2;
 import org.apache.spark.api.java.function.PairFunction;
 
@@ -23,9 +25,34 @@ public class SparkTest {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		
+	}
+	
+	public static void loadData() {
+		SparkConf conf = new SparkConf().setMaster("local").setAppName("My App");
+		JavaSparkContext sc = new JavaSparkContext(conf);
+		JavaRDD<String> lines = sc.parallelize(Arrays.asList("pandas", "i like pandas"));
+		
+		// 匿名内部类的方式传入函数
+		JavaRDD<String> error = lines.filter(new Function<String , Boolean>() {
+			public Boolean call(String x) {
+				return x.contains("error");
+			}
+		});
+		
+		// java8 lambda
+		JavaRDD<String> error2 = lines.filter(s -> s.contains("error"));
+		
+		
+	}
+	
+	public static void sparkMR() {
 		SparkConf conf = new SparkConf().setMaster("local").setAppName("My App");
 		JavaSparkContext sc = new JavaSparkContext(conf);
 		JavaRDD<String> input = sc.textFile("/opt/data/tmp/wordcount.txt");
+		
+		long c = input.count();
+		List<String> subList = input.take(10);
 
 		// Split up into words.
 		JavaRDD<String> words = input.flatMap(new FlatMapFunction<String, String>() {
